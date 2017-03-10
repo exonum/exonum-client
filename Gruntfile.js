d89acbb544a95282d1977889213c99088a9ab898
Ivan Bohuslavskyi <ivan.bohuslavskyi@xdev.re>
2017-03-10 14:03:50 +0200

    moved tests into grunt scenario

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,10 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        clean: {
             dist: {
-                src: './dist/<%= pkg.name %>.js',
-                dest: './dist/<%= pkg.name %>.min.js'
-            }
-        },
-        browserify: {
-            dist: {
-                src: './src/browser.js',
-                dest: './dist/<%= pkg.name %>.js'
+                src: ['./dist']
             }
         },
         jshint: {
@@ -22,13 +14,33 @@ module.exports = function(grunt) {
                 node: true,
                 funcscope: true
             }
+        },
+        mochaTest: {
+            options: {
+                reporter: 'spec'
+            },
+            src: ['./test/*.js']
+        },
+        browserify: {
+            dist: {
+                src: './src/browser.js',
+                dest: './dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            dist: {
+                src: './dist/<%= pkg.name %>.js',
+                dest: './dist/<%= pkg.name %>.min.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['jshint', 'browserify', 'uglify']);
+    grunt.registerTask('default', ['clean', 'jshint', 'mochaTest', 'browserify', 'uglify']);
 
 };
