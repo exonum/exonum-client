dd97219d11f32beca29c02aa293f757cbcd08c11
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-04-26 23:39:15 +0300

    Sequence types

---
+++ src/types/sequence.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const integers = require('./integers');
+const utils = require('./utils');
+
+const reservedFieldNames = [
+  'get',
+  'set',
+  'byteLength',
+  'hasFixedLength'
+];
+
+// throws on error
+function validateSpec (spec) {
+  spec.forEach((field, i) => {
+    if (!field.name) {
+      throw new Error('No field name specified for field #' + i);
+    }
+    if (reservedFieldNames.indexOf(field.name) >= 0) {
+      throw new Error('Reserved field name: ' + field.name);
+    }
+    if (!field.type) {
+      throw new Error('No field type specified for field #' + i);
+    }
+    if (!utils.isExonumType(field.type)) {
+      throw new Error('Invalid type: ' + field.type);
+    }
+  });
+
+  // TODO check field uniqueness
+}
+
+function Sequence (spec) {
+  'use strict';
+  validateSpec(spec);
+
+  const fieldNames = spec.map(f => f.name);
+  // Starting and ending positions of fields/segments
+  let startPos = [];
+  let endPos = [];
+  for (var i = 0; i < spec.length; i++) {
+    startPos[i] = (i === 0) ? 0 : endPos[i - 1];
+    endPos[i] = startPos[i] + (spec[i].type.hasFixedLength
+      ? spec[i].type.byteLength
+      : Segment.byteLength);
+  }
+  const fixedLength = endPos[spec.length - 1];
+
+  function SequenceType (obj) {
+    var i;
+    this.raw = [];
+
+    if (typeof obj === 'object' && arguments.length === 1) {
+      // Read object field-by-field
+      for (i = 0; i < spec.length; i++) {
+        if (obj[spec[i].name] !== undefined) {
+          this.set(spec[i].name, obj[spec[i].name]);
+        }
+      }
+    } else {
+      // Assume arguments are the fields
+      for (i = 0; i < arguments.length; i++) {
+        this.set(spec[i].name, arguments[i]);
+      }
+    }
+  }
+
+  SequenceType.prototype.get = function (name) {
+    var idx = fieldNames.indexOf(name);
+    if (idx >= 0) {
+      return this.raw[idx];
+    }
+    return undefined;
+  };
+
+  SequenceType.prototype.set = function (name, value) {
+    var idx = fieldNames.indexOf(name);
+    if (idx >= 0) {
+      var Type = spec[idx].type;
+      if (value instanceof Type) {
+        this.raw[idx] = value;
+      } else {
+        this.raw[idx] = new Type(value);
+      }
+    } else {
+      throw new Error('Unknown property: ' + name);
+    }
+  };
+
+  fieldNames.forEach(name => {
+    Object.defineProperty(SequenceType.prototype, name, {
+      enumerable: true,
+      configurable: true,
+      get: function () { return this.get(name); },
+      set: function (value) { this.set(name, value); }
+    });
+  });
+
+  function getByteLength () {
+    var len = 0;
+    for (var i = 0; i < spec.length; i++) {
+      if (spec[i].type.hasFixedLength) {
+        len += spec[i].type.byteLength;
+      } else {
+        var value = this.get(spec[i].name);
+        len += Segment.byteLength;
+        if (value) len += value.byteLength;
+      }
+    }
+    return len;
+  }
+
+  Object.defineProperty(SequenceType.prototype, 'byteLength', {
+    enumerable: true,
+    configurable: true,
+    get: getByteLength
+  });
+
+  SequenceType.prototype.serialize = function (buffer) {
+    if (!buffer) {
+      buffer = new Uint8Array(this.byteLength);
+    } else if (buffer.length !== this.byteLength) {
+      throw new Error('Unexpected buffer length: ' + buffer.length +
+        '; ' + this.byteLength + ' expected');
+    }
+
+    var ptr = fixedLength;
+    for (var i = 0; i < spec.length; i++) {
+      var value = this.get(spec[i].name);
+
+      if (spec[i].type.hasFixedLength) {
+        // Write the value in place
+        value.serialize(buffer.subarray(startPos[i], endPos[i]));
+      } else {
+        // Write the value as a segment pointer
+        var len = value.byteLength;
+        // Serialize value in heap
+        var heap = buffer.subarray(ptr, ptr + len);
+        value.serialize(heap);
+        // Serialize segment in place
+        var segment = new Segment(heap.byteOffset, len);
+        segment.serialize(buffer.subarray(startPos[i], endPos[i]));
+        ptr += len;
+      }
+    }
+    return buffer;
+  };
+
+  var hasFixedLength = spec.every(f => f.type.hasFixedLength);
+  utils.configureType(SequenceType, {
+    byteLength: hasFixedLength ? fixedLength : undefined
+  });
+
+  return SequenceType;
+}
+
+var Segment = Sequence([
+  { name: 'start', type: integers.Uint32 },
+  { name: 'length', type: integers.Uint32 }
+]);
+
+module.exports = {
+  Sequence: Sequence,
+  validateSpec: validateSpec
+};
---
+++ test/test.sequence.js
@@ -0,0 +1,224 @@
+'use strict';
+/* eslint-env node, mocha */
+
+const expect = require('chai').expect;
+const bigInt = require('big-integer');
+
+const Sequence = require('../src/types/sequence').Sequence;
+const integers = require('../src/types/integers');
+const Str = require('../src/types/string').Str;
+
+function expectInt (obj, expected) {
+  expect(integers.isInteger(obj)).to.be.true;
+  expect(obj.toString()).to.equal(expected.toString());
+}
+
+describe('Sequence', function () {
+  var Type = Sequence([
+    { name: 'foo', type: integers.Uint32 },
+    { name: 'bar', type: integers.Int64 }
+  ]);
+
+  var VarType = Sequence([
+    { name: 'str', type: Str },
+    { name: 'foo', type: integers.Uint8 }
+  ]);
+
+  var ComplexType = Sequence([
+    { name: 'a', type: integers.Int16 },
+    { name: 'b', type: VarType },
+    { name: 'c', type: Str }
+  ]);
+
+  describe('constructor', function () {
+    it('should parse spec', function () {
+      expect(Type).to.be.a('function');
+      // FIXME verify schema
+    });
+
+    it('should calculate hasFixedLength', function () {
+      expect(Type.hasFixedLength).to.be.true;
+      expect(VarType.hasFixedLength).to.be.false;
+    });
+
+    it('should calculate byteLength', function () {
+      expect(Type.byteLength).to.equal(12);
+      expect(VarType.byteLength).to.be.undefined;
+    });
+
+    it('should instantiate from arguments', function () {
+      var x = new Type(12, -344666);
+      expect(x).to.have.property('foo');
+      expect(x).to.have.property('bar');
+      expectInt(x.foo, 12);
+      expectInt(x.bar, -344666);
+    });
+
+    it('should instantiate from an object', function () {
+      var x = new Type({
+        foo: 12,
+        bar: -344666
+      });
+      expect(x).to.have.property('foo');
+      expect(x).to.have.property('bar');
+      expectInt(x.foo, 12);
+      expectInt(x.bar, -344666);
+    });
+
+    it('should support field writes with various coercible types', function () {
+      var x = new Type(12, -344666);
+      x.foo = 23;
+      expectInt(x.foo, 23);
+      x.foo = '42';
+      expectInt(x.foo, 42);
+      x.foo = bigInt('111');
+      expectInt(x.foo, 111);
+      x.foo = new integers.Uint32(57566);
+      expectInt(x.foo, 57566);
+    });
+
+    it('should support sequence-typed properties', function () {
+      var ComplexType = Sequence([
+        { name: 'complex', type: Type },
+        { name: 'simple', type: integers.Int64 }
+      ]);
+
+      var x = new ComplexType(
+        new Type(1, -2),
+        new integers.Int64(25)
+      );
+      expect(x).to.have.property('complex');
+      expect(x).to.have.property('simple');
+      expect(x).to.have.deep.property('complex.foo');
+      expect(x).to.have.deep.property('complex.bar');
+      expectInt(x.complex.foo, 1);
+      expectInt(x.complex.bar, -2);
+      expectInt(x.simple, 25);
+    });
+
+    it('should support object assignment of sequence-typed properties', function () {
+      var ComplexType = Sequence([
+        { name: 'complex', type: Type },
+        { name: 'simple', type: integers.Int64 }
+      ]);
+
+      var x = new ComplexType({
+        complex: {
+          foo: 1,
+          bar: -2
+        },
+        simple: 25
+      });
+      expectInt(x.complex.foo, 1);
+      expectInt(x.complex.bar, -2);
+      expectInt(x.simple, 25);
+    });
+
+    it('should support partial assignement through arguments', function () {
+      var x = new Type(11);
+      expectInt(x.foo, 11);
+      expect(x.bar).to.be.undefined;
+    });
+
+    it('should support partial assignement through object', function () {
+      var x = new Type({ bar: 11 });
+      expectInt(x.bar, 11);
+      expect(x.foo).to.be.undefined;
+    });
+  });
+
+  describe('get', function () {
+    it('should return values of defined properties', function () {
+      var x = new Type(1, -2);
+      expectInt(x.get('foo'), 1);
+      expectInt(x.get('bar'), -2);
+    });
+
+    it('should return undefined for undefined properties', function () {
+      var x = new Type(1, -2);
+      expect(x.get('bazz')).to.be.undefined;
+      expect(x.get('get')).to.be.undefined;
+      expect(x.get('set')).to.be.undefined;
+      expect(x.get('hasContsantLength')).to.be.undefined;
+    });
+  });
+
+  describe('set', function () {
+    it('should set defined properties', function () {
+      var x = new Type(1, -2);
+      x.set('foo', 5);
+      expectInt(x.foo, 5);
+      x.set('foo', '256765');
+      expectInt(x.foo, 256765);
+    });
+
+    it('should throw for undefined properties', function () {
+      var x = new Type(1, -2);
+      expect(() => x.set('bazz', 5)).to.throw(Error, /property/i);
+      expect(() => x.set('get', 5)).to.throw(Error, /property/i);
+      expect(() => x.set('set', 5)).to.throw(Error, /property/i);
+    });
+  });
+
+  describe('byteLength', function () {
+    it('should count segments in var-length types', function () {
+      var x = new VarType('ABC', 55);
+      expect(x.byteLength).to.equal(8 + 1 + 3);
+    });
+
+    it('should count all segments in embedded var-length types', function () {
+      var x = new ComplexType({
+        a: -10,
+        b: {
+          str: 'ABC',
+          foo: 55
+        },
+        c: 'f00'
+      });
+      expect(x.byteLength).to.equal(2 + (8 + (8 + 1 + 3)) + (8 + 3));
+    });
+  });
+
+  describe('serialize', function () {
+    it('should serialize a fixed-length type', function () {
+      var x = new Type(1, -2);
+      expect(x.serialize()).to.deep.equal(new Uint8Array([1, 0, 0, 0, 254, 255, 255, 255, 255, 255, 255, 255]));
+    });
+
+    it('should serialize a var-length type', function () {
+      var x = new VarType('ABC', 55);
+      expect(x.serialize()).to.deep.equal(new Uint8Array([
+        9, 0, 0, 0, // segment start
+        3, 0, 0, 0,  // segment length
+        55, // x.bar
+        65, 66, 67 // x.foo
+      ]));
+    });
+
+    it('should serialize a complex var-length type', function () {
+      var x = new ComplexType({
+        a: -10,
+        b: {
+          str: 'cabbage',
+          foo: 16
+        },
+        c: 'f00'
+      });
+      expect(x.serialize()).to.deep.equal(new Uint8Array([
+        // offset: contents
+        /*  0: */ 0xf6, 0xff, // x.a
+        /*  2: */ 18, 0, 0, 0, // segment for x.b
+        /*  6: */ 16, 0, 0, 0,
+        /* 10: */ 34, 0, 0, 0, // segment for x.c
+        /* 14: */ 3, 0, 0, 0,
+        // start x.b
+        /* 18: */ 27, 0, 0, 0, // segment for x.b.str
+        /* 22: */ 7, 0, 0, 0,
+        /* 26: */ 16, // x.b.foo
+        /* 27: */ 99, 97, 98, 98, 97, 103, 101, // x.b.str
+        // end x.b
+        /* 34: */ 102, 48, 48 // x.c
+      ]));
+    });
+  });
+});
