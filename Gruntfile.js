91a2cfa5aa3c4ce133f1cb52e73c66e16e924924
Ivan Bohuslavskyi <ivan.bohuslavskyi@xdev.re>
2018-12-07 17:44:49 +0200

    start work on map proof fixing

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,14 +63,14 @@ module.exports = function (grunt) {
           require: ['babel-register']
         },
         src: [
-          './test/sources/blockchain.js',
-          './test/sources/convertors.js',
-          // './test/sources/merkle-patricia-proof.js',
-          './test/sources/merkle-proof.js',
-          './test/sources/cryptography.js',
-          './test/sources/protobuf-message.js',
+          // './test/sources/blockchain.js',
+          // './test/sources/convertors.js',
+          './test/sources/merkle-patricia-proof.js',
+          // './test/sources/merkle-proof.js',
+          // './test/sources/cryptography.js',
+          // './test/sources/protobuf-message.js',
           // './test/sources/readme.js',
-          './test/sources/types.js'
+          // './test/sources/types.js'
         ]
       },
       all: {
@@ -79,14 +79,14 @@ module.exports = function (grunt) {
           require: ['babel-register']
         },
         src: [
-          './test/sources/blockchain.js',
-          './test/sources/convertors.js',
-          // './test/sources/merkle-patricia-proof.js',
-          './test/sources/merkle--proof.js',
-          './test/sources/cryptography.js',
-          './test/sources/protobuf-message.js',
+          // './test/sources/blockchain.js',
+          // './test/sources/convertors.js',
+          './test/sources/merkle-patricia-proof.js',
+          // './test/sources/merkle--proof.js',
+          // './test/sources/cryptography.js',
+          // './test/sources/protobuf-message.js',
           // './test/sources/readme.js',
-          './test/sources/types.js'
+          // './test/sources/types.js'
         ]
       }
     }
