ae25ab22b0f6f159c3a8f0558a396b5fc9c7600e
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-01-20 18:27:34 +0200

    Tinker with `babel` some more

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
       all: {
         options: {
           reporter: 'spec',
-          require: ['babel-register']
+          require: ['@babel/register']
         },
         src: ['./test/**/*.js']
       }
