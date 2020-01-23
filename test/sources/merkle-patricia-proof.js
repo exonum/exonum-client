a912830a75b6cb8a45562a5b2fae787f50fcb547
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-01-23 19:14:05 +0200

    Support raw keys in map proofs

--- test/sources/merkle-patricia-proof.js
+++ test/sources/merkle-patricia-proof.js
@@ -615,6 +615,25 @@ describe('MapProof', () => {
       expect(proof.merkleRoot).to.equal(expHash)
     })
 
+    it('should calculate hash for a single node with raw key', () => {
+      const key = '34264463370758a230017c5635678c9a39fa90a5081ec08f85de6c56243f4011'
+      const proof = new MapProof({
+        entries: [
+          { key, value: 100 }
+        ],
+        proof: []
+      }, MapProof.rawKey(PublicKey), Uint16)
+
+      const nodeHash = streamHash(
+        [4, 1],
+        key,
+        [0],
+        streamHash([0, 100, 0]) // Blob marker + little-endian encoding of the value
+      )
+      const expHash = streamHash([3], nodeHash)
+      expect(proof.merkleRoot).to.equal(expHash)
+    })
+
     it('should calculate hash for a single hashed node', () => {
       const key = '34264463370758a230017c5635678c9a39fa90a5081ec08f85de6c56243f4011'
       const proof = new MapProof({
